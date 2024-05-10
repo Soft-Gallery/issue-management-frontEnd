fef0cfed3eb085c66757c23b2c59c9acf16ae41f
Subin <71542970+dmstjdhdh@users.noreply.github.com>
2024-05-11 03:44:14 +0900

    feat: 사이드 바 애니메이션 추가

--- src/shared/components/Layout/SideBar.tsx
+++ src/shared/components/Layout/SideBar.tsx
@@ -1,21 +1,38 @@
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
-const SideBar = () => {
-  return (
-    <Container>
+interface ContainerProps {
+  isActive: boolean;
+}
 
+const SideBar: React.FC = () => {
+  const [isActive, setIsActive] = useState<boolean>(false);
+
+  return (
+    <Container
+      isActive={isActive}
+      onMouseEnter={() => setIsActive(true)}
+      onMouseLeave={() => setIsActive(false)}
+    >
+      <p>메뉴 1</p>
+      <p>메뉴 2</p>
+      <p>메뉴 3</p>
     </Container>
   )
 }
 
 export default SideBar;
 
-const Container = styled.div`
-  width: 250px;
+const Container = styled.div<ContainerProps>`
+  width: 240px;
   height: 100vh;
-  background-color: #FFFFFF;
-  color: #727A90;
   position: fixed;
   top: 0;
   left: 0;
+  padding: 0 30px;
+  transition: transform 1s;
+  flex-direction: column;
+  align-items: flex-start;
+  background-color: ${({ theme: { color } }) => color.white};
+  transform: ${({ isActive }) => isActive ? 'translateX(0)' : 'translateX(-100%)'};
 `;
