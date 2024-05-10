d9844d8b0211155def9c1e50dffea93c7c882b15
Subin <71542970+dmstjdhdh@users.noreply.github.com>
2024-05-11 03:22:11 +0900

    feat: Sidebar 설정

--- src/shared/components/Layout/Layout.tsx
+++ src/shared/components/Layout/Layout.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import styled from 'styled-components';
+import SideBar from './SideBar';
 
 const Layout = () => {
   return (
     <>
+      <SideBar />
       <LayoutContainer>
         <Outlet />
       </LayoutContainer>
---
+++ src/shared/components/Layout/SideBar.tsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+const SideBar = () => {
+  return (
+    <Container>
+
+    </Container>
+  )
+}
+
+export default SideBar;
+
+const Container = styled.div`
+  width: 250px;
+  height: 100vh;
+  background-color: #FFFFFF;
+  color: #727A90;
+  position: fixed;
+  top: 0;
+  left: 0;
+`;
