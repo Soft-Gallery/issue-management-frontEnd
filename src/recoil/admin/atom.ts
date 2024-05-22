9370aea9468addbadf06c96b265959615d58be5d
Subin <71542970+dmstjdhdh@users.noreply.github.com>
2024-05-22 16:25:22 +0900

    feat: 프로젝트를 추가하는 상태의 atom 추가

--- src/recoil/admin/atom.ts
+++ src/recoil/admin/atom.ts
@@ -1,6 +1,19 @@
 import { atom } from 'recoil';
 import { CURRENT_VIEW_STATES } from './constants/constants';
+import { Project } from '../../shared/types/project';
 export const adminPageViewState = atom<string>({
   key: 'currentViewState',
   default: CURRENT_VIEW_STATES.NONE,
 });
+
+export const adminPageAddProjectState = atom<Project>({
+  key: 'addProjectState',
+  default: {
+    title: '',
+    description: '',
+    pl: [],
+    dev: [],
+    tester: [],
+    issues: [],
+  },
+})
