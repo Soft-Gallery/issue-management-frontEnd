2ddfc422c1a16272acd139809a88fdc4aafce887
Subin <71542970+dmstjdhdh@users.noreply.github.com>
2024-05-23 00:44:51 +0900

    feat: PL의 정보를 담는 박스 컴포넌트 제작 진행

--- src/feature/admin/components/addproject/PLInfoItem.tsx
+++ src/feature/admin/components/addproject/PLInfoItem.tsx
@@ -1,55 +1,100 @@
-import React from 'react';
+import { plListDummy } from '../../../../dummy/plListDummy';
+import React, { useState } from 'react';
+import { PLUser } from '../../../../shared/types/user';
 import styled from 'styled-components';
 import ElementContainer from '../../../../shared/components/ElementContainer';
 
 const PLInfoItem = () => {
+  const [selectedPLs, setSelectedPLs] = useState<PLUser[]>([]);
+
+  const handlePLChange = (index: number) => (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedId = parseInt(e.target.value);
+    const updatedPLs = [...selectedPLs];
+    const pl = plListDummy.find(pl => pl.id === selectedId);
+    if (pl) {
+      updatedPLs[index] = pl;
+      setSelectedPLs(updatedPLs);
+    }
+  };
+
+  const addPL = () => {
+    setSelectedPLs(prevState => [...prevState, plListDummy[0]]);
+  };
+
+  const removePL = (index: number) => {
+    setSelectedPLs(prevState => {
+      const updatedPLs = [...prevState];
+      updatedPLs.splice(index, 1);
+      return updatedPLs;
+    });
+  };
+
   return (
     <ElementContainer>
       <ElementTitleText>PL 정보</ElementTitleText>
-      <ElementSubTitleText>프로젝트 제목</ElementSubTitleText>
-      <TitleTextInput placeholder="프로젝트 제목을 입력하세요" />
-      <ElementSubTitleText>프로젝트 설명</ElementSubTitleText>
-      <DescriptionTextInput placeholder="프로젝트 설명을 입력하세요" />
+      {selectedPLs.map((selectedPL, index) => (
+        <div key={index}>
+          <ElementSubTitleText>PL 선택</ElementSubTitleText>
+          <PLSelect value={selectedPL.id} onChange={handlePLChange(index)}>
+            <option value="">PL을 선택하세요</option>
+            {plListDummy.map(pl => (
+              <option key={pl.id} value={pl.id}>
+                {pl.name}
+              </option>
+            ))}
+          </PLSelect>
+          <RemoveButton onClick={() => removePL(index)}>Remove</RemoveButton>
+        </div>
+      ))}
+      <AddButton onClick={addPL}>Add PL</AddButton>
     </ElementContainer>
-  )
-}
+  );
+};
 
-const ElementTitleText = styled.text`
+const ElementTitleText = styled.div`
   display: flex;
   text-align: left;
   margin-bottom: 12px;
   font-size: 18px;
-  color: ${({ theme: { color } }) => color.black};
+  color: ${({ theme }) => theme.color.black};
 `;
 
-const ElementSubTitleText = styled.text`
+const ElementSubTitleText = styled.div`
   display: flex;
   text-align: left;
   margin-top: 12px;
   font-size: 14px;
-  color: ${({ theme: { color } }) => color.gray1};
+  color: ${({ theme }) => theme.color.gray1};
 `;
 
-const TitleTextInput = styled.input`
+const PLSelect = styled.select`
   width: 100%;
   box-sizing: border-box;
   padding: 8px;
   margin-top: 4px;
-  border: 1px solid ${({ theme: { color } }) => color.gray1};
+  border: 1px solid ${({ theme }) => theme.color.gray1};
   border-radius: 4px;
   font-size: 14px;
 `;
 
-const DescriptionTextInput = styled.input`
-  width: 100%;
-  box-sizing: border-box;
-  padding: 8px;
+const RemoveButton = styled.button`
   margin-top: 4px;
-  border: 1px solid ${({ theme: { color } }) => color.gray1};
+  padding: 8px 16px;
+  border: none;
   border-radius: 4px;
-  font-size: 14px;
-  height: 100px;
-  vertical-align: top;
+  background-color: ${({ theme }) => theme.color.red};
+  color: white;
+  cursor: pointer;
+`;
+
+const AddButton = styled.button`
+  margin-top: 8px;
+  padding: 8px 16px;
+  border: none;
+  border-radius: 4px;
+  background-color: ${({ theme }) => theme.color.green};
+  color: white;
+  cursor: pointer;
 `;
 
 export default PLInfoItem;
