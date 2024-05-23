f517ea82031e5aec067aed36c866597dba3fe062
Subin <71542970+dmstjdhdh@users.noreply.github.com>
2024-05-23 15:07:47 +0900

    fix: 로그인 페이지 다시 되돌리는 커밋

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,30 +1,9 @@
 import { useNavigate } from "react-router-dom";
-import React, { useState } from 'react';
-import { useSetRecoilState } from 'recoil';
-import { userRoleState } from '../recoil/atom';
+import React from 'react';
 const LoginPage = () => {
-  const [role, setRole] = useState<string>('guest');
-  const setUserRole = useSetRecoilState(userRoleState);
   const navigate = useNavigate();
 
-  const handleLogin = () => {
-    setUserRole(role);
-    navigate('/');
-  };
-
-  return (
-    <div>
-      <h2>Login Page</h2>
-      <select value={role} onChange={(e) => setRole(e.target.value)}>
-        <option value="guest">Guest</option>
-        <option value="admin">Admin</option>
-        <option value="dev">Developer</option>
-        <option value="pl">Project Leader</option>
-        <option value="tester">Tester</option>
-      </select>
-      <button onClick={handleLogin}>Login</button>
-    </div>
-  );
+  return <div><h1>로그인</h1></div>;
 }
 
 export default LoginPage;
