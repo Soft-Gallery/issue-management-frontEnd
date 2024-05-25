584f5d0ce6c08bd2e39907b8449769790491736a
sumin <sumin1667@gmail.com>
2024-05-25 16:33:15 +0900

    refactor: delete unnecssary import statement

--- src/recoil/atom.ts
+++ src/recoil/atom.ts
@@ -1,6 +1,5 @@
 import { atom } from 'recoil';
 import { USER_ROLE_STATES } from './constants/constants';
-import { ProjectCardItemType } from '../shared/types/project';
 
 export const userRoleState = atom<string>({
   key: 'userRoleState',
