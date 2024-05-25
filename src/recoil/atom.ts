58def6f39c839d01aebe7d6b1b9c1223c94cfd6c
sumin <sumin1667@gmail.com>
2024-05-25 16:22:52 +0900

    feat: create project(dashboard) page

--- src/recoil/atom.ts
+++ src/recoil/atom.ts
@@ -1,5 +1,6 @@
 import { atom } from 'recoil';
 import { USER_ROLE_STATES } from './constants/constants';
+import { ProjectCardItemType } from '../shared/types/project';
 
 export const userRoleState = atom<string>({
   key: 'userRoleState',
