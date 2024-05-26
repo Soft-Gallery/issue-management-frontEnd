974fd4f060634f09c8c00a922bc62b6a0fd0158b
Subin <71542970+dmstjdhdh@users.noreply.github.com>
2024-05-26 18:23:03 +0900

    feat: tester의 상태 관리 및 페이지 이동 구현

---
+++ src/recoil/tester/atom.ts
@@ -0,0 +1,7 @@
+import { atom } from 'recoil';
+import { TESTER_CURRENT_VIEW_STATES } from './constants/constants';
+
+export const testerPageViewState = atom<string>({
+  key: 'currentViewState',
+  default: TESTER_CURRENT_VIEW_STATES.ISSUE_BROWSE,
+});
